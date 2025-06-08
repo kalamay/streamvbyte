8f8cd0cf3c472c8e8a607ef6ee804de79de7361c
Jeremy Larkin <jeremylarkin@gmail.com>
2025-06-08 11:08:55 -0700

    fix unaligned write of keys

--- src/streamvbytedelta_x64_encode.c
+++ src/streamvbytedelta_x64_encode.c
@@ -29,7 +29,7 @@ static size_t streamvbyte_encode_SSE41_d1_init (const uint32_t* in, uint32_t cou
 	for (const uint32_t* end = &in[(count & ~7U)]; in != end; in += 8)
 	{
 		__m128i rawr0, r0, rawr1, r1, r2, r3;
-		size_t keys;
+		uint16_t keys;
 
 		rawr0 = _mm_loadu_si128((const __m128i*)&in[0]);
     r0 = Delta(rawr0, Prev);
@@ -43,7 +43,7 @@ static size_t streamvbyte_encode_SSE41_d1_init (const uint32_t* in, uint32_t cou
 		r2 = _mm_packus_epi16(r2, r3);
 		r2 = _mm_min_epi16(r2, mask_01); // convert 0x01FF to 0x0101
 		r2 = _mm_adds_epu16(r2, mask_7F00); // convert: 0x0101 to 0x8001, 0xFF01 to 0xFFFF
-		keys = (size_t)_mm_movemask_epi8(r2);
+		keys = (uint16_t)_mm_movemask_epi8(r2);
 
 		r2 = _mm_loadu_si128((const __m128i*)&shuf_lut[(keys << 4) & 0x03F0]);
 		r3 = _mm_loadu_si128((const __m128i*)&shuf_lut[(keys >> 4) & 0x03F0]);
@@ -55,7 +55,7 @@ static size_t streamvbyte_encode_SSE41_d1_init (const uint32_t* in, uint32_t cou
 		_mm_storeu_si128((__m128i *)dataPtr, r1);
 		dataPtr += len_lut[keys >> 8];
 
-		*((uint16_t*)keyPtr) = (uint16_t)keys;
+		memcpy(keyPtr, &keys, 2);
 		keyPtr += 2;
 	}
   prev = (uint32_t)_mm_extract_epi32(Prev,3);
